dcb7b4662afbb76c07637f3e1d46f2725e3efdd0
Gianluca D'Arcangelo <gianluca.darcangelo@micegroup.it>
2024-10-05 00:10:03 +0200

    Aggiunta build corretta

--- docs/portfolio-gianluca/utilities/interface/icon.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface Icon {
-  id: number;
-  name: string;
-  image: string;
-  position: { x: number; y: number };
-}
--- docs/portfolio-gianluca/utilities/interface/open-window.interface.ts
+++
@@ -1,6 +0,0 @@
-export interface OpenWindow {
-  id: number;
-  title: string;
-  icon: string;
-  isMinimized: boolean;
-}
